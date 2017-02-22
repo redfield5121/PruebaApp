de7b3d5a266eaf60b49549a597f3da666f344511
redfield5121 <erick150399@hotmail.com>
2017-02-22 15:19:12 -0600

    Add files via upload

---
+++ Prueba/app/src/main/java/ipn/cecyt9/edu/prueba/MainActivity.java
@@ -0,0 +1,13 @@
+package ipn.cecyt9.edu.prueba;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
